720ac9b7cc93f106ec3d1e5b92ca647e62b3fced
Cedrick Regis Jr. <cedrick.hayag@gmail.com>
2025-12-03 11:48:56 +0300

    Inserted default categories into Categories table

--- Models/Category.cs
+++ Models/Category.cs
@@ -1,11 +1,11 @@
 ﻿namespace StationaryManagement.Models;
+
 public class Category
 {
     public int CategoryId { get; set; }
-    public string CategoryName { get; set; } = null!;
-    public string? Description { get; set; } // <-- add this
+    public string CategoryName { get; set; } = string.Empty;
+    public string? Description { get; set; }
 
     // Navigation property for items in this category
     public ICollection<StationeryItem> Items { get; set; } = new List<StationeryItem>();
 }
-//a
