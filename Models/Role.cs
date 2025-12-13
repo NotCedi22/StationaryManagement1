3ced9e7a66211f8e1642104f7763e875e3c09ef4
Cedrick Regis Jr. <cedrick.hayag@gmail.com>
2025-12-13 13:59:06 +0300

    Role Model added ReportsToRoleID

--- Models/Role.cs
+++ Models/Role.cs
@@ -5,15 +5,24 @@
         public int RoleId { get; set; }
         public string RoleName { get; set; } = null!;
         public string? Description { get; set; }
+        
+        // Foreign key for role hierarchy (which role this role reports to)
         public int? ReportsToRoleId { get; set; }
 
-        // Add this property for approval hierarchy
+        // Approval hierarchy - whether this role can approve requests
         public bool CanApprove { get; set; } = false;
 
         // Navigation properties
+        // The role that this role reports to (navigation property for ReportsToRoleId)
         public Role? ReportsTo { get; set; }
+        
+        // Collection of roles that report to this role
         public ICollection<Role> DirectReports { get; set; } = new List<Role>();
+        
+        // Employees with this role
         public ICollection<Employee> Employees { get; set; } = new List<Employee>();
+        
+        // Role threshold for approval limits
         public RoleThreshold? RoleThreshold { get; set; }
     }
 }
